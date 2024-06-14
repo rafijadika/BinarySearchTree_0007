f6acc9acb71e880ce76a159a76e3deccee63c5f9
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2024-06-14 14:00:14 +0700

    3

--- BinarySearchTree_0007/BinarySearchTree_0007.cpp
+++ BinarySearchTree_0007/BinarySearchTree_0007.cpp
@@ -17,3 +17,37 @@ public:
         rightchild = r;
     }
 };
+
+class BinaryTree
+{
+public:
+    Node* ROOT;
+
+    BinaryTree()
+    {
+        ROOT = nullptr; // Initializing ROOT to null
+    }
+
+    void insert(string element) // Insert a node in the binary search tree
+    {
+        Node* newNode = new Node(element, nullptr, nullptr); // Allocate memory for the new node
+
+        Node* parent = nullptr;
+        Node* currentNode = nullptr;
+        search(element, parent, currentNode); // Locate the node which will be the parent of the node to be inserted
+
+        if (parent == nullptr) // if the parent is NULL (Tree is empty)
+        {
+            ROOT = newNode; // Mark the new node as ROOT
+            return; // Exit
+        }
+
+        if (element < parent->info) // If the value in the data field of the new node is less than that of the parent
+        {
+            parent->leftchild = newNode; // Make the left child of the parent point to the new node
+        }
+        else if (element > parent->info) // If the value in the data field of the new node is greater than that of the parent
+        {
+            parent->rightchild = newNode; // Make the right child of the parent point to the new node
+        }
+    }
