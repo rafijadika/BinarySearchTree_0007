25bc51176d5c5d6f94c37bb76a4488cb1175a733
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2024-06-14 14:00:46 +0700

    4

--- BinarySearchTree_0007/BinarySearchTree_0007.cpp
+++ BinarySearchTree_0007/BinarySearchTree_0007.cpp
@@ -51,3 +51,18 @@ public:
             parent->rightchild = newNode; // Make the right child of the parent point to the new node
         }
     }
+
+    void search(string element, Node*& parent, Node*& currentNode)
+    {
+        // This function searches the currentNode of the specified Node as well as the current Node of its parent
+        currentNode = ROOT;
+        parent = nullptr;
+        while ((currentNode != nullptr) && (currentNode->info != element))
+        {
+            parent = currentNode;
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+                currentNode = currentNode->rightchild;
+        }
+    }
