56771514e0f31106f9d4ecba6789dbb0dd4b3aa1
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2024-06-14 13:59:46 +0700

    2

--- BinarySearchTree_0007/BinarySearchTree_0007.cpp
+++ BinarySearchTree_0007/BinarySearchTree_0007.cpp
@@ -1,4 +1,19 @@
 #include <iostream>
 #include <string>
 using namespace std;
- 
+
+class Node
+{
+public:
+    string info;
+    Node* leftchild;
+    Node* rightchild;
+
+    // Constructor for the Node class
+    Node(string i, Node* l, Node* r)
+    {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
+};
